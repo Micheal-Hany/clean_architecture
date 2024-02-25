eef346cbc384138473f43fc99a94c37f3355aa1a
micheal00000 <michealhany991@gmail.com>
2024-02-25 22:11:28 +0200

    clean arch

---
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/Features/home/domain/entity/book_entity.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Faluire, List<BookEntity>>> feachFeaturedBook();
+  Future<Either<Faluire, List<BookEntity>>> feachNewsedBook();
+}
